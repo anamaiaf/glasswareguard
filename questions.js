e47441ede31a78c0256102a2ec43f511936941b8
anamaia <analeticiamaiafontes@gmail.com>
2023-11-19 10:18:17 -0300

    first commit

---
+++ questions.js
@@ -0,0 +1,47 @@
+export default [
+    {
+      question: "O que é uma proveta e qual é sua função em um laboratório?",
+      answers: [
+        { option: "Recipiente para armazenar líquidos", correct: false },
+        { option: "Instrumento para medir volumes líquidos, apesar de não ser o mais preciso", correct: true },
+        { option: "Ferramenta para triturar sólidos", correct: false },
+        { option: "Tubo para realizar reações químicas", correct: false }
+      ],
+    },
+    {
+      question: "Quais são os diferentes tipos de tubos de ensaio?",
+      answers: [
+        { option: "Tubo de ensaio de vidro e tubo de ensaio de plástico", correct: false },
+        { option: "Tubo de ensaio de fundo chato e tubo de ensaio de fundo redondo", correct: true },
+        { option: "Tubo de ensaio de destilação e tubo de ensaio de ebulição", correct: false },
+        { option: "Tubo de ensaio de cultura e tubo de ensaio de fermentação", correct: false }
+      ],
+    },
+    {
+      question: "Quais são os principais riscos associados ao uso inadequado de pipetas em um laboratório?",
+      answers: [
+        { option: "Quebra do vidro", correct: false },
+        { option: "Derramameto de substâncias", correct: false },
+        { option: "Conteminação cruzada", correct: false },
+        { option: "Todas as alternativas", correct: true }
+      ],
+    },
+    {
+      question: "Como você deve proceder ao manusear ácidos ou bases fortes em um laboratório?",
+      answers: [
+        { option: "Utilizar luvas e óculos de proteção", correct: true },
+        { option: "Misturar ácidos e bases para neutralizar", correct: false },
+        { option: "Manusear sem precauções, pois são comuns em laboratórios", correct: false },
+        { option: "Armazenar próximo a materiais inflamáveis", correct: false}
+      ],
+    },
+    {
+      question: "Descreva as precauções de segurança que devem ser tomadas ao operar equipamentos elétricos em um ambiente de laboratório.",
+      answers: [
+        { option: "Usar equipamentos com fios desencapados", correct: false },
+        { option: "Manusear equipamentos elétricos com as mãos molhadas.", correct: false },
+        { option: "Seguir as instruções do fabricante e usar equipamentos com fiação adequada", correct: true },
+        { option: "Utilizar qualquer extensão elétrica disponível no momento", correct: false}
+      ],
+    }
+  ];
